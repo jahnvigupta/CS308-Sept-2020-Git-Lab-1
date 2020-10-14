2574ff134eca0a35ec6879cd096b9469d38fc0ec
jahnvi <jahnvigupta11@gmail.com>
2020-10-14 18:32:57 +0530

    Added DEBUG: to force conflict

--- Lab 2/sqrt.c
+++ Lab 2/sqrt.c
@@ -8,16 +8,16 @@ positive number. */
 int main(int argc, char* argv[]) {
 	
 	if (argc != 2) {
-		printf("Usage: sqrt input\n\n");
+		printf("DEBUG: Usage: sqrt input\n\n");
 		exit(-1);
 	}
 
 	int input = atoi(argv[1]);
 	if(input<0)
-		printf("Sqrt of %d (negative number) is %fi\n",input,sqrt(-1*input));
+		printf("DEBUG: Sqrt of %d (negative number) is %fi\n",input,sqrt(-1*input));
 	else
-		printf("Sqrt of %d is %f\n",input,sqrt(input));
-	printf("End of program. Exiting");
+		printf("DEBUG: Sqrt of %d is %f\n",input,sqrt(input));
+	printf("DEBUG: End of program. Exiting");
 	return(0);
 
 } // end main
